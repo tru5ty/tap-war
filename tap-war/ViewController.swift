76cb20831450aa0d1726f8bbbec7a58928535b55
Nathan McGuire <tru5ty@users.noreply.github.com>
2015-12-08 14:09:17 +1100

    add all UI. Add all functionality. Finished

--- tap-war/ViewController.swift
+++ tap-war/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    @IBOutlet weak var orangeLabel: UILabel!
+    @IBOutlet weak var purpleLabel: UILabel!
+    
+    @IBOutlet weak var endGameButton: UIButton!
+    
+    @IBOutlet weak var orangeWinLbl: UILabel!
+    @IBOutlet weak var purpleWinLbl: UILabel!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        score = 0
+        purpleLabel.text = "\(score)"
+        orangeLabel.text = "\(score)"
+        
+        orangeLabel.transform = CGAffineTransformMakeRotation(3.14)
+        orangeWinLbl.transform = CGAffineTransformMakeRotation(3.14)
+        
+        endGameButton.hidden = true
+        orangeWinLbl.hidden = true
+        purpleWinLbl.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func orangeButtonTouched(sender: AnyObject) {
+        score++
+        purpleLabel.text = "\(score)"
+        orangeLabel.text = "\(score)"
+        
+        testScore()
+    }
+    
+    @IBAction func purpleButtonTouched(sender: AnyObject) {
+        score--
+        purpleLabel.text = "\(score)"
+        orangeLabel.text = "\(score)"
+        
+        testScore()
+    }
+    
+    func testScore() {
+        if score >= 10 {
+            endGameButton.hidden = false
+            orangeWinLbl.hidden = false
+            purpleWinLbl.hidden = false
+            
+            orangeWinLbl.text = "Winner!"
+            purpleWinLbl.text = "Loser..."
+        } else if score <= -10 {
+            endGameButton.hidden = false
+            orangeWinLbl.hidden = false
+            purpleWinLbl.hidden = false
+            
+            orangeWinLbl.text = "Loser..."
+            purpleWinLbl.text = "Winner!"
+        }
+    }
 
+    @IBAction func endGameAction(sender: AnyObject) {
+        endGameButton.hidden = true
+        orangeWinLbl.hidden = true
+        purpleWinLbl.hidden = true
+        
+        score = 0
+        purpleLabel.text = "\(score)"
+        orangeLabel.text = "\(score)"
+    }
 }
 
