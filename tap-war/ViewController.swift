cd399d1d7f608a225ecce6721121613522d63a20
Nathan McGuire <tru5ty@users.noreply.github.com>
2015-12-08 13:28:28 +1100

    Initial Commit

---
+++ tap-war/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tap-war
+//
+//  Created by Nathan McGuire on 8/12/2015.
+//  Copyright © 2015 Nathan McGuire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
